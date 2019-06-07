36d8d796211030bfb9d79b5fc1bb2eef54ff11e4
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-06-07 19:25:14 -0300

    refactor: class to function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { BottomNavigation, BottomNavigationAction, Paper } from '@material-ui/core'
 import { Add, Home, ChevronLeft } from '@material-ui/icons'
@@ -6,53 +6,26 @@ import { Add, Home, ChevronLeft } from '@material-ui/icons'
 import Route from './routes/Route'
 import './App.scss'
 
-const styles = {
-  root: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-    paddingBottom: 15,
-    borderTop: '1px solid #ccc'
-  },
-  paper: {
-    paddingTop: 30,
-    paddingBottom: 100,
-    background: 'transparent !important',
-    minHeight: '100vh'
-  },
-};
+function App(){
+  const [state, setState] = useState({value: 0})
 
-class App extends React.Component {
-  state = {
-    value: 0,
-  };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
-    return (
-      <>
-        <Paper square className={classes.paper}>
-          <Route />
-        </Paper>
-        <BottomNavigation
-          value={value}
-          onChange={this.handleChange}
-          showLabels
-          className={classes.root}
-        >
-          <BottomNavigationAction label="Voltar" icon={<ChevronLeft />} onClick={() => window.history.back()} />
-          <BottomNavigationAction label="Inicio" icon={<Home />} onClick={() => window.location = '/#/restaurants'} />
-          <BottomNavigationAction label="Criar" icon={<Add />} onClick={() => window.location = '/#/restaurants/create'} />
-        </BottomNavigation>
-      </>
-    );
-  }
+  return (
+    <>
+      <Paper square className="paper">
+        <Route />
+      </Paper>
+      <BottomNavigation
+        value={state.value}
+        showLabels
+        className="bottom-navigation"
+        onChange={(event, value) => setState({ value }) }
+      >
+        <BottomNavigationAction label="Voltar" icon={<ChevronLeft />} onClick={() => window.history.back()} />
+        <BottomNavigationAction label="Inicio" icon={<Home />} onClick={() => window.location = '/#/restaurants'} />
+        <BottomNavigationAction label="Criar" icon={<Add />} onClick={() => window.location = '/#/restaurants/create'} />
+      </BottomNavigation>
+    </>
+  )
 }
 
-export default withStyles(styles)(App)
+export default App
