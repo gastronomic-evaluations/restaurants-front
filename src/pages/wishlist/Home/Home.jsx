6c34e401243548c599050e118cf4da004601a710
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-07-11 22:07:11 -0300

    feat: delete a wish

--- src/pages/wishlist/Home/Home.jsx
+++ src/pages/wishlist/Home/Home.jsx
@@ -7,25 +7,31 @@ import { Delete } from '@material-ui/icons'
 
 import './home.scss'
 
-function CardHome({ wish }) {
+function CardHome({ wish, exclude }) {
   const { name, _id } = wish
 
   return (
     <li className="wish">
-      <Link className="wish__link" to={`/wishlist/edit/${_id}`}>{ name }</Link>
-      <Delete className="wish__delete" />
+      <Link className="wish__link" to={`/wishlist/edit/${_id}`}>{name}</Link>
+      <Delete className="wish__delete" onClick={() => exclude(_id)} />
     </li>
   )
 }
 
 function Home() {
-  const {data: wishlist, loaded} = useFetch('/wishlist')
+  const {data: wishlist, setData, loaded} = useFetch('/wishlist')
+
+  function exclude(id) {
+    fetch(`${process.env.REACT_APP_API_URL}/wishlist/${id}`, { method: 'DELETE' })
+    const data = wishlist.filter(({_id}) => _id !== id)
+    setData(data)
+  }
   
   return (
     <section className="wishlist">
       {
         loaded
-          ? wishlist.map(wish => <CardHome key={wish._id} wish={wish} />)
+          ? wishlist.map(wish => <CardHome key={wish._id} exclude={exclude} wish={wish} />)
           : <Loader />
       }
 
